75fe9299f337834a027deabce6fb0473d245c2f4
oni-shiro <sinchan.saha99@gmail.com>
2023-01-23 14:36:41 +0530

    Initial Project Commit

---
+++ automation/src/test/java/com/camunda/automation/AutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camunda.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
